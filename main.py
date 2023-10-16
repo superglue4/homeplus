9324172dcfb7e537dad89f9214dfe6919f6d2879
jungryeolshin <sjr4746@gmail.com>
2023-10-16 14:00:53 +0900

    안녕 메시지 출력

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+from fastapi import FastAPI
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+app = FastAPI()
+
+@app.get("/")
+
+async def welcome() -> dict:
+    return {
+        "message" : "안녕 !"
+    }
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+
