3db192e5e86013a49c3472585230f57c7fbcc187
jungryeolshin <sjr4746@gmail.com>
2023-10-17 10:07:36 +0900

    진자 템플릿 테스트

--- router/coupon.py
+++ router/coupon.py
@@ -1,20 +1,29 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Depends
+from fastapi.templating import Jinja2Templates
+from starlette.templating import _TemplateResponse
+
 from models.coupon import Coupon
 coupon_router = APIRouter()
 
 coupon_list = []
 
+templates = Jinja2Templates(directory="templates/")
+
 
 @coupon_router.post("/coupon")
-async def post_coupon(coupon: Coupon) -> dict:
+async def post_coupon(request: Request, coupon: Coupon) -> _TemplateResponse:
     coupon_list.append(coupon)
-    return {
-        "message": "쿠폰 추가 완료!"
-    }
+    return templates.TemplateResponse("post.html", {
+        "request": request,
+        "coupon": coupon
+
+    })
 
 
 @coupon_router.get("/coupon")
-async def get_coupon() -> dict:
-    return {
-        "message": coupon_list
-    }
+async def get_coupon(request:Request) -> _TemplateResponse:
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "coupon": coupon_list
+
+    })
